38e0196d12ae7c61f34e2990a5bf4eb7af6589a0
divyanjalimadhupada <137045587+divyanjalimadhupada@users.noreply.github.com>
2023-06-20 13:26:06 +0530

    Added Target Find Logic

--- ArraysNew/src/NewClass.java
+++ ArraysNew/src/NewClass.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,28 @@
  * @author hp
  */
 public class NewClass {
+    
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a[]=new int[5];
+        int i,isthere=0;
+        for(i=0;i<a.length;i++){
+        System.out.println("enter elements ");
+        a[i]=scan.nextInt();
+        }
+        System.out.println("enter target");
+        int target=scan.nextInt();
+        for(i=0;i<a.length;i++){
+            if(a[i]==target){
+                isthere=1;
+                break;
+            }
+        }if(isthere==1){
+            System.out.println("target present");
+        }else{
+                System.out.println("no target element");
+                }
+    }
+    
     
 }
